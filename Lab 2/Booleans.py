4e1bedf5e019da76729a6f5d4635a84ef2083f5a
aruuzhkka <ar_batyrbayeva@kbtu.kz>
2025-02-06 13:13:33 +0600

    Create Booleans.py

---
+++ Lab 2/Booleans.py
@@ -0,0 +1,19 @@
+#Ex1(The following code example would print the data type of x, what data type would that be?):
+print(10 > 9)
+#Answer: True
+
+#Ex2(The statement below would print a Boolean value, which one?):
+print(10 == 9)
+#Answer: False
+
+#Ex3(The statement below would print a Boolean value, which one?):
+print(10 < 9)
+#Answer: False
+
+#Ex4(The statement below would print a Boolean value, which one?):
+print(bool("abc"))
+#Answer: True
+
+#Ex5(The statement below would print a Boolean value, which one?):
+print(bool(0))
+#Answer: False
