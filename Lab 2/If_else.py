291bfd14546e9b55c1e11248787ca41021ada54c
aruuzhkka <ar_batyrbayeva@kbtu.kz>
2025-02-06 13:17:47 +0600

    Create If_else.py

---
+++ Lab 2/If_else.py
@@ -0,0 +1,53 @@
+#Ex1(Print "Hello World" if a is greater than b.):
+a = 50
+b = 10
+if a>b:
+    print("Hello World")
+
+#Ex2(Print "Hello World" if a is not equal to b.):
+a = 50
+b = 10
+if a!=b:
+    print("Hello World")
+
+#Ex3(Print "Yes" if a is equal to b, otherwise print "No".):
+a = 50
+b = 10
+if a==b:
+    print("Yes")
+else:
+    print("No")
+
+#Ex4(Print "1" if a is equal to b, print "2" if a is greater than b, otherwise print "3".):
+a = 50
+b = 10
+if a==b:
+    print("1")
+elif a>b:
+    print("2")
+else:
+    print("3")
+
+#Ex5(Print "Hello" if a is equal to b, and c is equal to d.):
+if a == b and c == d:
+    print("Hello")
+
+#Ex6(Print "Hello" if a is equal to b, or if c is equal to d.):
+if a == b or c == d:
+    print("Hello")
+
+#Ex7(Complete the code block, print "YES" if 5 is larger than 2. Hint: remember the indentation.):
+if 5 > 2:
+    print("YES")
+
+#Ex8(Use the correct one line short hand syntax to print "YES" if a is equal to b, otherwise print("NO").):
+a = 2
+b = 5
+print("YES") if a==b else print("NO")
+
+#Ex9(Use an if statement to print "YES" if either a or b is equal to c.):
+a = 2
+b = 50
+c = 2
+if a==c or b==c:
+    print("YES")
