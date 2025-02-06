e8b8e307b60652f2dd14a9792cadf131a309c98d
aruuzhkka <ar_batyrbayeva@kbtu.kz>
2025-02-06 13:21:12 +0600

    Create Operators.py

---
+++ Lab 2/Operators.py
@@ -0,0 +1,18 @@
+#Ex1(Multiply 10 with 5, and print the result.):
+print(10*5)
+
+#Ex2(Divide 10 by 2, and print the result.):
+print(10/2)
+
+#Ex3(Use the correct membership operator to check if "apple" is present in the fruits object.):
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+    print("Yes, apple is a fruit!")
+
+#Ex4(Use the correct comparison operator to check if 5 is not equal to 10.):
+if 5!=10:
+    print("5 and 10 is not equal")
+
+#Ex5(Use the correct logical operator to check if at least one of two statements is True.):
+if 5 == 10 or 4 == 4:
+    print("At least one of the statements is true")
