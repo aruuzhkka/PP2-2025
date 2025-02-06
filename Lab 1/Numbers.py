6c50760b4313f1492c44a850e68950d18bd5b73a
aruuzhkka <ar_batyrbayeva@kbtu.kz>
2025-02-06 13:08:16 +0600

    Create Numbers.py

---
+++ Lab 1/Numbers.py
@@ -0,0 +1,9 @@
+#Ex1(Insert the correct syntax to convert x into a floating point number.):
+x = 5
+x =float (x)
+#Ex2(Insert the correct syntax to convert x into a integer.):
+x = 5.5
+x = int(x)
+#Ex3(Insert the correct syntax to convert x into a complex number.):
+x = 5
+x = complex (x)
