796bda1e1623e92f72a233e694de2af22c01b0c0
aruuzhkka <ar_batyrbayeva@kbtu.kz>
2025-02-06 13:06:44 +0600

    Create Data_types.py

---
+++ Lab 1/Data_types.py
@@ -0,0 +1,34 @@
+#Ex1(The following code example would print the data type of x, what data type would that be?):
+x = 5
+print(type(x))
+#Answer: int(integer)
+
+#Ex2(The following code example would print the data type of x, what data type would that be?):
+x = "Hello World"
+print(type(x))
+#Answer: str(string)
+
+#Ex3(The following code example would print the data type of x, what data type would that be?):
+x = 20.5
+print(type(x))
+#Answer: float
+
+#Ex4(The following code example would print the data type of x, what data type would that be?):
+x = ["apple", "banana", "cherry"]
+print(type(x))
+#Answer: list
+
+#Ex5(The following code example would print the data type of x, what data type would that be?):
+x = ("apple", "banana", "cherry")
+print(type(x))
+#Answer: tuple
+
+#Ex6(The following code example would print the data type of x, what data type would that be?):
+x = {"name" : "John", "age" : 36}
+print(type(x))
+#Answer: dict
+
+#Ex7(The following code example would print the data type of x, what data type would that be?):
+x = True
+print(type(x))
+#Answer: bool
