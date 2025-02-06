3a37d4207ef47d958a836d839aaf861c69a924e1
aruuzhkka <ar_batyrbayeva@kbtu.kz>
2025-02-06 13:16:22 +0600

    Create ForLoops.py

---
+++ Lab 2/ForLoops.py
@@ -0,0 +1,21 @@
+#Ex1(Loop through the items in the fruits list.):
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+#Ex2(In the loop, when the item value is "banana", jump directly to the next item.):
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+     continue
+  print(x)
+
+#Ex3(Use the range function to loop through a code set 6 times.):
+for x in range(6):
+  print(x)
+
+#Ex4(Exit the loop when x is "banana".):
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+     break
+  print(x)
